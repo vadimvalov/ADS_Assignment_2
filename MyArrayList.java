654392a1df734c9f16ad2cf3399a8a30e6732c55
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-04-21 22:15:38 +0600

    Final tests.
    Idk, it is outputting addresses for me instead of objects, so I used "for" loop to print the contents.
    
    I also debugged MyArrayList. lol I forgot to name it MyArrayList.java
    
    I guess it is working for now, however it has a lot to improve this project.

--- MyArrayList.java
+++ MyArrayList.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class MyArrayList<T> implements MyList<T> {
     private int size;
     private Object[] elements;
-
+    
     public MyArrayList(){
         elements= new Object[10];
         size=0;
@@ -17,53 +17,56 @@ public class MyArrayList<T> implements MyList<T> {
     @Override
     public boolean contains(Object o){
         for (int i=0; i<size; i++){
-            if (elements[i].equals(O)){
+            if (elements[i].equals(o)){
                 return true;
             }
         }   return false;
     }
 
     @Override
-    public void add(T item){
-        if (size == elements.length){
-            Object[] newElements = new Object[elements.length *2];
-            System.arraycopy(elements, 0, newElements, 0);
-            elements= newElements;
+    public void add(T item) {
+    if (size == elements.length) {
+        Object[] newElements = new Object[elements.length * 2];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
         }
-        elements[size] = item;
-        size++;
+        elements = newElements;
     }
+    elements[size++] = item;
+}
 
     @Override
-    public void add(T item, int index) {
-         if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException();
-        }
-        if (size == elements.length) {
-            Object[] newElements = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newElements, 0, index);
-            newElements[index] = item;
-            System.arraycopy(elements, index, newElements, index + 1, size - index);
-            elements = newElements;
-        } else {
-            System.arraycopy(elements, index, elements, index + 1, size - index);
-            elements[index] = item;
-        }
-        size++;
+  public void add(T item, int index) {
+    if (index < 0 || index > size) {
+        throw new IndexOutOfBoundsException();
     }
+    if (size == elements.length) {
+        Object[] newElements = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, index);
+        newElements[index] = item;
+        System.arraycopy(elements, index, newElements, index + 1, size - index);
+        elements = newElements;
+    } else {
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+    }
+    size++;
+}
 
     @Override
-    public boolean remove(T item){
-        for (int i=0; i < size; i++){
-            if (elements[i].equals(item)) {
-                System.arraycopy(elements, i+1, elements, size - i - 1);
-                size--;
-                elements[size] = null;
-                return true;
+    public boolean remove(T item) {
+    for (int i = 0; i < size; i++) {
+        if (elements[i].equals(item)) {
+            for (int j = i + 1; j < size; j++) {
+                elements[j - 1] = elements[j];
             }
+            elements[size - 1] = null;
+            size--;
+            return true;
         }
-        return false;
     }
+    return false;
+}
 
     @Override
     public T remove(int index){
--- test.java
+++ test.java
@@ -1,3 +1,51 @@
+import java.util.Random;
+
 public class test {
-    //input code for test
+    public static void main(String[] args) {
+        MyList<String> list1 = new MyArrayList<>();
+        
+        list1.add("10");
+        list1.add("20");
+        list1.add("30");
+
+        System.out.println("Size of MyArrayList: " + list1.size());
+
+        System.out.println("Contents of MyArrayList: " );
+        for (int i = 0; i < list1.size(); i++) {
+        System.out.print(list1.get(i) + " ");
+     }
+        System.out.println();
+
+        list1.remove(1);
+
+        System.out.print("Contents of MyArrayList after removing second element: ");
+        for (int i = 0; i < list1.size(); i++) {
+        System.out.print(list1.get(i) + " ");
+        }
+        System.out.println();
+
+
+        
+        MyList<Integer> list2 = new MyLinkedList<>();
+        
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            list2.add(rand.nextInt(100));
+        }
+        
+        System.out.println("Size of MyLinkedList: " + list2.size());
+        System.out.println("Contents of MyLinkedList: ");
+        for (int i = 0; i < list2.size(); i++) {
+        System.out.print(list2.get(i) + " ");
+        }
+        System.out.println();
+        
+        list2.sort();
+        
+        System.out.println("Contents of MyLinkedList after sorting: ");
+        for (int i = 0; i < list2.size(); i++) {
+        System.out.print(list2.get(i) + " ");
+        }
+        System.out.println();
+    }
 }
